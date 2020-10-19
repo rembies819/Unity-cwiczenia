93a8f8761ee4edb2b236094263ed0baeb3855fcc
rembies819 <32666917+rembies819@users.noreply.github.com>
2020-10-19 12:23:23 +0200

    Zad2

---
+++ Proj. gier w środowisku Unity Lab3/Assets/Scripts/TestRigidBody.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRigidBody : MonoBehaviour
+{
+    public float delta = 10.0f;  
+    public float speed = 1.0f;
+    private Vector3 PozycjaStartowa;
+
+    void Start()
+    {
+        PozycjaStartowa = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 v = PozycjaStartowa;
+        v.x += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
+
