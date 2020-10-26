b663a23478d48442ec22529fedb156dc8ea4732d
rembies819 <32666917+rembies819@users.noreply.github.com>
2020-10-26 12:07:25 +0100

    Dokonczenie zadan z lab3

---
+++ Proj. gier w środowisku Unity Lab3/Assets/Scripts/DetektorKolizji.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetektorKolizji : MonoBehaviour
+{
+    void OnCollisionEnter(Collision przeszkoda)
+    {
+        if (przeszkoda.gameObject.tag == "Przeszkoda")
+        {
+            Debug.Log("Wykryto kolizje z przeszkoda");
+        }
+    }
+}
---
+++ Proj. gier w środowisku Unity Lab3/Assets/Scripts/GenerowanieObiektow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerowanieObiektow : MonoBehaviour
+{
+    public GameObject myPrefab;
+
+    void Start()
+    {
+        for (int y = 0; y < 10; y++)
+        {
+            Instantiate(myPrefab, Random.insideUnitSphere * 5 + transform.position, Random.rotation);
+        }
+    }
+}
