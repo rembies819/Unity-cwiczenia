0fff5a6cb61d8a93015a5d6814104c6c869e0508
rembies819 <32666917+rembies819@users.noreply.github.com>
2020-11-23 07:21:56 +0100

    Lab5

---
+++ Proj. gier w środowisku Unity Lab5/Assets/Scripts/Launcher.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player zderzył się z kostką.");
+        }
+    }
+}
